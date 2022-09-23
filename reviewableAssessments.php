ad05d62c95415a8ba4591216600e9cefe0030fba
mvl22 <martin@lucas-smith.co.uk>
2022-09-23 02:13:15 +0100

    Remove an unwanted formCompleteText, though not normally seen due to redirect anyway

--- reviewableAssessments.php
+++ reviewableAssessments.php
@@ -1523,6 +1523,7 @@ abstract class reviewableAssessments extends frontControllerApplication
 			'autofocus' => true,
 			'display' => 'tables',
 			'displayColons' => false,
+			'formCompleteText' => false,
 		));
 		if ($data) {
 			$form->heading ('p', "This form will start a <strong>new</strong> {$this->settings['description']} based on the <a href=\"{$this->baseUrl}/submissions/{$cloneId}/\">existing {$this->settings['description']}</a> you specified.");
